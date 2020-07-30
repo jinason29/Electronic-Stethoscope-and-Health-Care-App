1394800fe49f62f2ced9f497ead4703131f3532d
jinason29 <68379712+jinason29@users.noreply.github.com>
2020-07-30 15:12:51 +0900

    Update audio_python.py

--- audio_python.py
+++ audio_python.py
@@ -46,7 +46,7 @@ temp_high = (temp_reg_12bit & 0x00ff)
 
  
 
-# Convert to temp from page 6 of datasheet
+# temp measuring
 
 temp = (((temp_high * 256) + temp_low)>>4)
 
@@ -66,29 +66,15 @@ temp_C2 = float(temp) * 0.0780
 
 time.sleep(1)
 
-temp_C3 = 36.5
+sy = temp_C1 + temp_C2 
 
-time.sleep(1)
-
-temp_C4 = 36.5
-
-time.sleep(0.5)
-
-temp_C5 = 37.5
-
-time.sleep(0.5)
-
-sy = temp_C1 + temp_C2 + temp_C3 +temp_C4 + temp_C5
-
-syh = sy / 5
-
- 
+syh = sy / 2
 
 time.sleep(1/10)
 
 tmp = "%3.2f" % (syh)
 
-print('{} ¡ÆC'.format(tmp))
+print('{} Â°C'.format(tmp))
 
 time.sleep(1/10)
 
@@ -96,7 +82,7 @@ time.sleep(1/10)
 
 print("tmp finished")
 
- 
+ #heart sound measuring
 
 form_1 = pyaudio.paInt16 # 16-bit resolution
 
@@ -359,58 +345,9 @@ elif (state == 6):
  
 
     fName = 'noise.m4a'
-
  
 
-elif (state == 7):
-
- 
-
-    fName = 'sja.wav'
-
- 
-
-elif (state == 8):
-
- 
-
-    fName = 'sja1.wav'
-
- 
-
-elif (state == 9):
-
- 
-
-    fName = 'sja2.wav'
-
- 
-
-elif (state == 10):
-
- 
-
-    fName = 'test.wav'
-
- 
-
-elif (state == 11):
-
- 
-
-    fName = 'test.wav'
-
- 
-
-elif (state == 12):
-
- 
-
-    fName = 'ttest.wav'
-
- 
-
-elif (state ==13) :
+elif (state ==7) :
 
      fName = 'test1.wav'
 
@@ -420,11 +357,7 @@ audio = AudioSegment.from_file (fName)
 
  
 
- 
 
- 
-
- 
 
  
 
@@ -494,12 +427,6 @@ if (fName [-3:] == 'wav'):
 
  
 
- 
-
- 
-
- 
-
 Ts = 1 / fs
 
  
@@ -564,11 +491,6 @@ y = d1 * 3
 
  
 
- 
-
- 
-
- 
 
  
 
@@ -929,7 +851,7 @@ else:
  
 
 beatPerMinute = 60/Period
-
+#BPM measuring
  
 
  
@@ -952,11 +874,11 @@ if (len(s1AndS2Locs)<3):
 
  
 
-    disease = "Á¤»óÀÔ´Ï´Ù."
+    disease = "ì •ìƒìž…ë‹ˆë‹¤."
 
  
 
-    advice = "Çö »óÅÂ¸¦ À¯ÁöÇÏ¼¼¿ä!"
+    advice = "í˜„ ìƒíƒœë¥¼ ìœ ì§€í•˜ì„¸ìš”!"
 
  
 
@@ -1008,11 +930,11 @@ else:
 
  
 
-            disease = "½ÉºÎÀü, Çù½ÉÁõ(°ü»óµ¿¸ÆÁúÈ¯), ½É±Ù°æ»ö(°ü»óµ¿¸ÆÁúÈ¯) ¿¹»ó"
+            disease = "ì‹¬ë¶€ì „, í˜‘ì‹¬ì¦(ê´€ìƒë™ë§¥ì§ˆí™˜), ì‹¬ê·¼ê²½ìƒ‰(ê´€ìƒë™ë§¥ì§ˆí™˜) ì˜ˆìƒ"
 
  
 
-            advice = "°È±â, °¡º­¿î »êÃ¥, ÀÚÀü°Å Å¸±â, ¼ö¿µ µîÀÇ À¯»ê¼Ò ¿îµ¿À» °í°­µµ¿Í º¸Åë °­µµ·Î ¹ø°¥¾Æ°¡¸ç ÇÏ¼¼¿ä.  °¡½¿ÀÌ ¶Ù¸ç ¾à°£ È£ÈíÀÌ °¡ºüÁö°í ¼Ò·®ÀÇ ¶¡ÀÌ ³ª´Â Àû´çÇÑ À¯»ê¼Ò ¿îµ¿Àº ÁÁÁö¸¸ ±Ù·Â ¿îµ¿ÀÌ³ª °ú°ÝÇÑ ¿îµ¿Àº Áö¾çÇÏ¿©¾ß ÇÕ´Ï´Ù. ¶ÇÇÑ ¸é¿ª°­È­¸¦ ½ÃÄÑÁÖ´Â ºí·çº£¸®¸¦ ²ÙÁØÈ÷ ¼·ÃëÇØÁÖ¼¼¿ä!."
+            advice = "ê±·ê¸°, ê°€ë²¼ìš´ ì‚°ì±…, ìžì „ê±° íƒ€ê¸°, ìˆ˜ì˜ ë“±ì˜ ìœ ì‚°ì†Œ ìš´ë™ì„ ê³ ê°•ë„ì™€ ë³´í†µ ê°•ë„ë¡œ ë²ˆê°ˆì•„ê°€ë©° í•˜ì„¸ìš”.  ê°€ìŠ´ì´ ë›°ë©° ì•½ê°„ í˜¸í¡ì´ ê°€ë¹ ì§€ê³  ì†ŒëŸ‰ì˜ ë•€ì´ ë‚˜ëŠ” ì ë‹¹í•œ ìœ ì‚°ì†Œ ìš´ë™ì€ ì¢‹ì§€ë§Œ ê·¼ë ¥ ìš´ë™ì´ë‚˜ ê³¼ê²©í•œ ìš´ë™ì€ ì§€ì–‘í•˜ì—¬ì•¼ í•©ë‹ˆë‹¤. ë˜í•œ ë©´ì—­ê°•í™”ë¥¼ ì‹œì¼œì£¼ëŠ” ë¸”ë£¨ë² ë¦¬ë¥¼ ê¾¸ì¤€ížˆ ì„­ì·¨í•´ì£¼ì„¸ìš”!."
 
  
 
@@ -1040,11 +962,11 @@ else:
 
  
 
-            disease = "±Þ¼º ½É±Ù°æ»ö, °íÇ÷¾Ð, ´ëµ¿¸Æ°ü ÇùÂøÁõ ¿¹»ó"
+            disease = "ê¸‰ì„± ì‹¬ê·¼ê²½ìƒ‰, ê³ í˜ˆì••, ëŒ€ë™ë§¥ê´€ í˜‘ì°©ì¦ ì˜ˆìƒ"
 
  
 
-            advice = "¿ªµµ³ª ¾¾¸§ ½Ç³»°ñÇÁ µî°ú °°ÀÌ ÂªÀº ¼ø°£¿¡ °Ý·ÄÇÏ°Ô ÈûÀ» ¾²°Å³ª È£ÈíÀ» Á¤ÁöÇÑ »óÅÂ¿¡¼­ ÇàÇÏ´Â ¿îµ¿Àº ¼öÃà±â Ç÷¾Ð°ú ÀÌ¿Ï±â Ç÷¾ÐÀÇ »ó½Â ¹ÝÀÀÀÌ Å©°í ¸»ÃÊ Ç÷°ü ÀúÇ×µµ Áõ°¡½ÃÅ´À¸·Î ÇÇÇÏ´Â °ÍÀÌ ÁÁ½À´Ï´Ù. ±×·¯¹Ç·Î Ç÷¾×¼øÈ¯À» ¿øÈ°ÇÏ°Ô ÇÏ´Â °¡º­¿î ½ºÆ®·¹Äª, °È±â, ÀÚÀü°Å Å¸±â µîÀÇ ¿îµ¿À» ÃßÃµµå¸³´Ï´Ù. ¶ÇÇÑ Æ÷È­ Áö¹æÀÌ Àû°í ºÒÆ÷È­Áö¹æ»êÀÌ ¸¹Àº µîÇª¸¥ »ý¼±, ¿Ã¸®ºêÀ¯ µîÀ» ¼·ÃëÇÏ´Â °ÍÀÌ ÁÁÀ¸¸ç Ä®·ýÀÌ ¸¹Àº ºÎÃß, ½Ã±ÝÄ¡, Å°À§¸¦ ²ÙÁØÈ÷ ¼·ÃëÇØ ÁÖ¼¼¿ä!"
+            advice = "ì—­ë„ë‚˜ ì”¨ë¦„ ì‹¤ë‚´ê³¨í”„ ë“±ê³¼ ê°™ì´ ì§§ì€ ìˆœê°„ì— ê²©ë ¬í•˜ê²Œ íž˜ì„ ì“°ê±°ë‚˜ í˜¸í¡ì„ ì •ì§€í•œ ìƒíƒœì—ì„œ í–‰í•˜ëŠ” ìš´ë™ì€ ìˆ˜ì¶•ê¸° í˜ˆì••ê³¼ ì´ì™„ê¸° í˜ˆì••ì˜ ìƒìŠ¹ ë°˜ì‘ì´ í¬ê³  ë§ì´ˆ í˜ˆê´€ ì €í•­ë„ ì¦ê°€ì‹œí‚´ìœ¼ë¡œ í”¼í•˜ëŠ” ê²ƒì´ ì¢‹ìŠµë‹ˆë‹¤. ê·¸ëŸ¬ë¯€ë¡œ í˜ˆì•¡ìˆœí™˜ì„ ì›í™œí•˜ê²Œ í•˜ëŠ” ê°€ë²¼ìš´ ìŠ¤íŠ¸ë ˆì¹­, ê±·ê¸°, ìžì „ê±° íƒ€ê¸° ë“±ì˜ ìš´ë™ì„ ì¶”ì²œë“œë¦½ë‹ˆë‹¤. ë˜í•œ í¬í™” ì§€ë°©ì´ ì ê³  ë¶ˆí¬í™”ì§€ë°©ì‚°ì´ ë§Žì€ ë“±í‘¸ë¥¸ ìƒì„ , ì˜¬ë¦¬ë¸Œìœ  ë“±ì„ ì„­ì·¨í•˜ëŠ” ê²ƒì´ ì¢‹ìœ¼ë©° ì¹¼ë¥¨ì´ ë§Žì€ ë¶€ì¶”, ì‹œê¸ˆì¹˜, í‚¤ìœ„ë¥¼ ê¾¸ì¤€ížˆ ì„­ì·¨í•´ ì£¼ì„¸ìš”!"
 
  
 
@@ -1072,11 +994,11 @@ else:
 
  
 
-          disease = "½ÉºÎÀü, °ü»óµ¿¸ÆÁúÈ¯ ¿¹»ó"
+          disease = "ì‹¬ë¶€ì „, ê´€ìƒë™ë§¥ì§ˆí™˜ ì˜ˆìƒ"
 
  
 
-          advice = "°íÇ÷¾Ð, ´ç´¢, ½ºÆ®·¹½º µîÀÌ µ¿¸Æ°æÈ­ÁõÀ» ÀçÃËÇÏ´Â ¿äÀÎÀÔ´Ï´Ù. µû¶ó¼­ ±Ý¿¬°ú Ã¼ÁßÁ¶Àý, Á¤»ó Ç÷¾Ð À¯Áö, ±×¸®°í ¾Æ½ºÇÇ¸° º¹¿ëÀ» ÀØÁö ¸»¾Æ¾ß ÇÕ´Ï´Ù. ¶ÇÇÑ Ç÷¾ÐÀ» ³ôÀÏ ¼ö ÀÖ´Â ³ªÆ®·ý°ú ¾ËÄÚ¿ÃÀ» Áö¾çÇÏ¸ç ¸¶½Ã´õ¶óµµ ÇÏ·ç¿¡ ¸ÆÁÖ 1Äµ, ¼ÒÁÖ 1~2ÀÜÀ¸·Î Á¦ÇÑÇØ¾ß ÇÕ´Ï´Ù!"
+          advice = "ê³ í˜ˆì••, ë‹¹ë‡¨, ìŠ¤íŠ¸ë ˆìŠ¤ ë“±ì´ ë™ë§¥ê²½í™”ì¦ì„ ìž¬ì´‰í•˜ëŠ” ìš”ì¸ìž…ë‹ˆë‹¤. ë”°ë¼ì„œ ê¸ˆì—°ê³¼ ì²´ì¤‘ì¡°ì ˆ, ì •ìƒ í˜ˆì•• ìœ ì§€, ê·¸ë¦¬ê³  ì•„ìŠ¤í”¼ë¦° ë³µìš©ì„ ìžŠì§€ ë§ì•„ì•¼ í•©ë‹ˆë‹¤. ë˜í•œ í˜ˆì••ì„ ë†’ì¼ ìˆ˜ ìžˆëŠ” ë‚˜íŠ¸ë¥¨ê³¼ ì•Œì½”ì˜¬ì„ ì§€ì–‘í•˜ë©° ë§ˆì‹œë”ë¼ë„ í•˜ë£¨ì— ë§¥ì£¼ 1ìº”, ì†Œì£¼ 1~2ìž”ìœ¼ë¡œ ì œí•œí•´ì•¼ í•©ë‹ˆë‹¤!"
 
           # simgeungyungsaek / simbeujun
 
@@ -1094,7 +1016,7 @@ if (beatPerMinute <= 60):
 
  
 
-        heartage = "´ç½ÅÀÇ ½ÉÀå ³ªÀÌ´Â 20´ë ÀÔ´Ï´Ù."
+        heartage = "ë‹¹ì‹ ì˜ ì‹¬ìž¥ ë‚˜ì´ëŠ” 20ëŒ€ ìž…ë‹ˆë‹¤."
 
  
 
@@ -1102,7 +1024,7 @@ elif (beatPerMinute <= 65):
 
  
 
-        heartage = "´ç½ÅÀÇ ½ÉÀå ³ªÀÌ´Â 30´ë ÀÔ´Ï´Ù."
+        heartage = "ë‹¹ì‹ ì˜ ì‹¬ìž¥ ë‚˜ì´ëŠ” 30ëŒ€ ìž…ë‹ˆë‹¤."
 
  
 
@@ -1110,7 +1032,7 @@ elif(beatPerMinute <= 70):
 
  
 
-        heartage = "´ç½ÅÀÇ ½ÉÀå ³ªÀÌ´Â 40´ë ÀÔ´Ï´Ù."
+        heartage = "ë‹¹ì‹ ì˜ ì‹¬ìž¥ ë‚˜ì´ëŠ” 40ëŒ€ ìž…ë‹ˆë‹¤."
 
  
 
@@ -1118,7 +1040,7 @@ elif(beatPerMinute <= 75):
 
  
 
-        heartage = "´ç½ÅÀÇ ½ÉÀå ³ªÀÌ´Â 50´ë ÀÔ´Ï´Ù."
+        heartage = "ë‹¹ì‹ ì˜ ì‹¬ìž¥ ë‚˜ì´ëŠ” 50ëŒ€ ìž…ë‹ˆë‹¤."
 
  
 
@@ -1126,7 +1048,7 @@ elif(beatPerMinute <= 80):
 
  
 
-        heartage = "´ç½ÅÀÇ ½ÉÀå ³ªÀÌ´Â 60´ë ÀÔ´Ï´Ù."
+        heartage = "ë‹¹ì‹ ì˜ ì‹¬ìž¥ ë‚˜ì´ëŠ” 60ëŒ€ ìž…ë‹ˆë‹¤."
 
  
 
@@ -1134,7 +1056,7 @@ else:
 
  
 
-        heartage = "´ç½ÅÀÇ ½ÉÀå ³ªÀÌ´Â 70´ë ÀÔ´Ï´Ù."
+        heartage = "ë‹¹ì‹ ì˜ ì‹¬ìž¥ ë‚˜ì´ëŠ” 70ëŒ€ ìž…ë‹ˆë‹¤."
 
  
 
@@ -1202,1287 +1124,7 @@ print(heartage)
 
  
 
-print('{} ¡ÆC'.format(tmp))
-
- 
-
-print(nowDatetime) # 2019-10-02 12:11:32
-
- 
-
- 
-
- 
-
-os.remove("test1.wav")
-
- 
-
- 
-
-db = MySQLdb.connect('localhost', 'root', '1234', 'python_test',charset = "utf8")
-
- 
-
- 
-
- 
-
-cur = db.cursor()
-
- 
-
- 
-
- 
-
-datalist = [[s3Result, s4Result, beatPerMinute, disease, advice, heartage, tmp, nowDatetime]]
-
- 
-
- 
-
- 
-
-cur.executemany("INSERT INTO fin_3 VALUE(%s, %s, %s, %s, %s, %s, %s, %s)", datalist)
-
- 
-
- 
-
- 
-
-cur.execute('SELECT * FROM fin_3')
-
- 
-
- 
-
- 
-
-data = cur.fetchall()
-
- 
-
- 
-
- 
-
-print(data)
-
- 
-
- 
-
- 
-
-db.commit()
-
-import pyaudio
-
-#import smbus
-
-import wave
-
-#import time
-
-import os
-
-from RPi import GPIO
-
- 
-
-#!/usr/bin/python
-
-import smbus as smbus
-
-import time
-
- 
-
-I2C_BUS = 1
-
- 
-
-bus = smbus.SMBus(I2C_BUS)
-
- 
-
-# 7 bit address (will be left shifted to add the read write bit)
-
-DEVICE_ADDRESS = 0x48 
-
-time.sleep(1/10)
-
-# Read the temp register
-
-temp_reg_12bit = bus.read_word_data(DEVICE_ADDRESS, 0)
-
-time.sleep(1/10)
-
-temp_low = (temp_reg_12bit & 0xff00) >> 8
-
-temp_high = (temp_reg_12bit & 0x00ff)
-
- 
-
-# Convert to temp from page 6 of datasheet
-
-temp = (((temp_high * 256) + temp_low)>>4)
-
-time.sleep(1/10)
-
-# Handle negative temps
-
-if temp > 0x7FF:
-
- temp = temp-4096;
-
-temp_C1 = float(temp) * 0.0780
-
-time.sleep(1)
-
-temp_C2 = float(temp) * 0.0780
-
-time.sleep(1)
-
-temp_C3 = 36.5
-
-time.sleep(1)
-
-temp_C4 = 36.5
-
-time.sleep(0.5)
-
-temp_C5 = 37.5
-
-time.sleep(0.5)
-
-sy = temp_C1 + temp_C2 + temp_C3 +temp_C4 + temp_C5
-
-syh = sy / 5
-
- 
-
-time.sleep(1/10)
-
-tmp = "%3.2f" % (syh)
-
-print('{} ¡ÆC'.format(tmp))
-
-time.sleep(1/10)
-
- 
-
-print("tmp finished")
-
- 
-
-form_1 = pyaudio.paInt16 # 16-bit resolution
-
- 
-
-chans = 1 # 1 channel
-
- 
-
-samp_rate = 44100 # 44.1 kHz sampling rate
-
- 
-
-chunk = 4096 # 2 ^ 12 samples for buffer
-
- 
-
-record_secs = 3 # seconds to record
-
- 
-
-dev_index = 2 # device index found by p.get_device_info_by_index (ii)
-
- 
-
-wav_output_filename = 'test1.wav' # name of .wav file
-
- 
-
- 
-
- 
-
-audio = pyaudio.PyAudio () # create pyaudio instantiation
-
- 
-
- 
-
- 
-
-# create pyaudio stream
-
- 
-
- 
-
-stream = audio.open (format = form_1, rate = samp_rate, channels = chans, \
-
- 
-
-                    input_device_index = dev_index, input = True, \
-
- 
-
-                    frames_per_buffer = chunk)
-
- 
-
-print ("recording")
-
- 
-
-frames = []
-
- 
-
- 
-
- 
-
-# loop through stream and append audio chunks to frame array
-
- 
-
-for ii in range(0, int ((samp_rate / chunk) * record_secs)):
-
- 
-
-    data = stream.read(chunk)
-
- 
-
-    frames.append(data)
-
- 
-
- 
-
- 
-
-print ("finished recording")
-
- 
-
- 
-
- 
-
-# stop the stream, close it, and terminate the pyaudio instantiation
-
- 
-
-stream.stop_stream()
-
- 
-
-stream.close()
-
- 
-
-audio.terminate()
-
- 
-
- 
-
- 
-
-# save the audio frames as .wav file
-
- 
-
-wavefile = wave.open(wav_output_filename, 'wb')
-
- 
-
-wavefile.setnchannels(chans)
-
- 
-
-wavefile.setsampwidth(audio.get_sample_size (form_1))
-
- 
-
-wavefile.setframerate(samp_rate)
-
- 
-
-wavefile.writeframes(b''. join(frames))
-
- 
-
-wavefile.close() 
-
- 
-
- 
-
- 
-
-import pydub
-
-from pydub import AudioSegment
-
-import soundfile as sf
-
-import mysql.connector
-
-from mysql.connector import Error
-
-from mysql.connector import errorcode
-
-import numpy as np
-
- 
-
-#from scipy.signal import find_peaks
-
- 
-
-from scipy.io import wavfile
-
-from numpy import fft as fft
-
-from scipy import signal as s
-
- 
-
-from matplotlib import pyplot as plt
-
- 
-
-from scipy import io as io
-
- 
-
- 
-
-import datetime
-
-import MySQLdb
-
- 
-
- 
-
- 
-
-import matplotlib as mpl
-
- 
-
-from matplotlib import pyplot as pl
-
- 
-
-import os
-
- 
-
-state = 2
-
- 
-
- 
-
-if (state == 1):
-
- 
-
-    fName = 'normal.m4a'
-
- 
-
-elif (state == 2):
-
- 
-
-    fName = 's3.m4a'
-
- 
-
-elif (state == 3):
-
- 
-
-    fName = 's4.m4a'
-
- 
-
-elif (state == 4):
-
- 
-
-    fName = 's3 and s4.m4a'
-
- 
-
-elif (state == 5):
-
- 
-
-    fName = 'micTest.wav'
-
- 
-
-elif (state == 6):
-
- 
-
-    fName = 'noise.m4a'
-
- 
-
-elif (state == 7):
-
- 
-
-    fName = 'sja.wav'
-
- 
-
-elif (state == 8):
-
- 
-
-    fName = 'sja1.wav'
-
- 
-
-elif (state == 9):
-
- 
-
-    fName = 'sja2.wav'
-
- 
-
-elif (state == 10):
-
- 
-
-    fName = 'test.wav'
-
- 
-
-elif (state == 11):
-
- 
-
-    fName = 'test.wav'
-
- 
-
-elif (state == 12):
-
- 
-
-    fName = 'ttest.wav'
-
- 
-
-elif (state ==13) :
-
-     fName = 'test1.wav'
-
- 
-
-audio = AudioSegment.from_file (fName)
-
- 
-
- 
-
- 
-
- 
-
- 
-
-d_modified = []
-
- 
-
- 
-
- 
-
-if (fName [-3:] == 'm4a'):
-
- 
-
-    # print ("m4a")
-
- 
-
-    audio.export (fName, format = "wav")
-
- 
-
-    [d, fs] = sf.read(fName)
-
- 
-
-    for i in d:
-
- 
-
-        d_modified.append(i [0])
-
- 
-
-    for i in d:
-
- 
-
-        d_modified.append(i [1])
-
- 
-
- 
-
- 
-
-if (fName [-3:] == 'wav'):
-
- 
-
-    # print ("wav")
-
- 
-
-    # [fs, d] = wavfile.read (fName)
-
- 
-
-    [d, fs] = sf.read(fName)
-
- 
-
-    d_modified = d
-
- 
-
- 
-
- 
-
- 
-
- 
-
-Ts = 1 / fs
-
- 
-
-N = len(d)
-
- 
-
-t = np.asarray(list(range(0, N)))
-
- 
-
-t = t * Ts
-
- 
-
-n = np.asarray(list(range(1, N + 1)))
-
- 
-
-tau = 2 ** 16.55
-
- 
-
- 
-
- 
-
-if (len(d) <60000 + int(tau)):
-
- 
-
-    d1 = np.asarray(d)
-
- 
-
-else:
-
- 
-
-    d1 = np.asarray(d_modified [60000: 60000 + int(tau)])
-
- 
-
- 
-
- 
-
-N1 = len(d1)
-
- 
-
-t = np.asarray(list(range(0, N1)))
-
- 
-
-t = t * Ts
-
- 
-
-y = d1 * 3
-
- 
-
- 
-
- 
-
- 
-
- 
-
-Y = fft.fft(y)
-
- 
-
-Y = fft.fftshift(Y)
-
- 
-
-n = np.asarray (list(range (0, N1)))
-
- 
-
-n = n-N1 / 2
-
- 
-
-f_hat = n / N1
-
- 
-
-f0 = f_hat * fs
-
- 
-
-Wn = np.asarray([20,150])
-
- 
-
-BPF_order = 3
-
- 
-
-fn = fs / 2
-
- 
-
-ftype = 'bandpass'
-
- 
-
-[b, a] = s.butter(BPF_order, Wn / fn, ftype)
-
- 
-
- 
-
- 
-
- 
-
- 
-
-y2 = s.lfilter(b, a, y)
-
- 
-
-Y2 = fft.fft(y2)
-
- 
-
-Y2 = fft.fftshift(Y2)
-
- 
-
- 
-
- 
-
- 
-
- 
-
-y = y * 2
-
- 
-
-y2 = y2 * 2
-
- 
-
- 
-
- 
-
-io.wavfile.write('Sound_before_filter.wav', fs, y)
-
- 
-
-io.wavfile.write('Sound_after_filter.wav', fs, y2)
-
- 
-
- 
-
- 
-
-N = 50
-
- 
-
-x = np.zeros(N-1)
-
- 
-
-x = np.insert(x, 0,1, axis = 0)
-
- 
-
-h1 = s.lfilter(b, a, x)
-
- 
-
- 
-
- 
-
- 
-
- 
-
-ffName = "IR_% d_th_order_fc_% d_Hz_length_% d.out"%(BPF_order, Wn [0], Wn [1])
-
- 
-
-f = open(ffName, "a")
-
- 
-
-for i in range(N):
-
- 
-
-    ff = "% f \ n"% h1 [i]
-
- 
-
-    f.write(ff)
-
- 
-
-f.close()
-
- 
-
- 
-
- 
-
-abs_y2 = abs(y2)
-
- 
-
-[locs, pks] = s.find_peaks(abs_y2, height = np.max(abs_y2) / 5)
-
- 
-
- 
-
- 
-
- 
-
- 
-
-pks =(pks.get('peak_heights'))
-
- 
-
-plt.scatter(locs, pks, c = 'r')
-
- 
-
-plt.plot(abs_y2)
-
- 
-
-# plt.show ()
-
- 
-
- 
-
- 
-
-[locs2, pks2] = s.find_peaks(pks);
-
- 
-
-pks2 =(pks2.get('peak_heights'))
-
- 
-
- 
-
- 
-
-realPksLocs = []
-
- 
-
- 
-
- 
-
-for i in locs2:
-
-    
-
-    realPksLocs.append(locs[i])
-
- 
-
- 
-
- 
-
-rmv = []
-
- 
-
- 
-
- 
-
-for i in range(1,len(realPksLocs)):
-
- 
-
-    if (realPksLocs[i]-realPksLocs[i-1]<2500):
-
- 
-
-        rmv.append (i)
-
- 
-
- 
-
- 
-
-for index in sorted(rmv, reverse = True):
-
- 
-
-    del realPksLocs[index]
-
- 
-
- 
-
- 
-
-realPks = []
-
- 
-
- 
-
- 
-
-for i in realPksLocs:
-
- 
-
-    realPks.append(abs_y2 [i])
-
- 
-
- 
-
- 
-
-s1AndS2 = []
-
- 
-
-for i in realPks:
-
- 
-
-    if (i> np.max (realPks) * 0.6):
-
- 
-
-        s1AndS2.append(i)
-
- 
-
-s1AndS2Locs = []
-
- 
-
- 
-
- 
-
-for i in range(len(realPks)):
-
-    
-
-    if (realPks[i]>np.max(realPks)*0.6):
-
- 
-
-        s1AndS2Locs.append(realPksLocs[i])
-
- 
-
- 
-
- 
-
-if (len(s1AndS2Locs)<2):
-
- 
-
-    Period = 1;
-
- 
-
-elif (len(s1AndS2Locs)<3):
-
-    
-
-    Period = (s1AndS2Locs[1]-s1AndS2Locs[0])*Ts
-
- 
-
-else:
-
- 
-
-    if (s1AndS2[0]>s1AndS2[1]):
-
- 
-
-        i = 0
-
- 
-
-    else:
-
- 
-
-        i = 1
-
- 
-
-    Period = (t[s1AndS2Locs [i + 2]])-t[s1AndS2Locs [i]]
-
- 
-
- 
-
- 
-
-beatPerMinute = 60/Period
-
- 
-
- 
-
- 
-
-if (len(s1AndS2Locs)<3):
-
- 
-
-    print("normal")
-
- 
-
-    s3Result = "X"
-
- 
-
-    s4Result = "X"
-
- 
-
-    disease = "Á¤»óÀÔ´Ï´Ù."
-
- 
-
-    advice = "Çö »óÅÂ¸¦ À¯ÁöÇÏ¼¼¿ä!"
-
- 
-
-else:
-
- 
-
-    for j in range(realPksLocs[0]):
-
- 
-
-        if (s1AndS2Locs[i] == realPksLocs[j]):
-
- 
-
-            startInd = j
-
- 
-
-        if (s1AndS2Locs[i + 2] == realPksLocs[j]):
-
- 
-
-            endInd = j
-
- 
-
-            break
-
- 
-
-    if (endInd-startInd == 3):
-
- 
-
-        if abs(realPksLocs[endInd-1]-realPksLocs[endInd]) <abs(realPksLocs[endInd-2]-realPksLocs[endInd-1]):
-
- 
-
-            #print ("s4")
-
- 
-
-            s3Result = "X"
-
- 
-
-            s4Result = "O"
-
- 
-
-            disease = "½ÉºÎÀü, Çù½ÉÁõ(°ü»óµ¿¸ÆÁúÈ¯), ½É±Ù°æ»ö(°ü»óµ¿¸ÆÁúÈ¯) ¿¹»ó"
-
- 
-
-            advice = "°È±â, °¡º­¿î »êÃ¥, ÀÚÀü°Å Å¸±â, ¼ö¿µ µîÀÇ À¯»ê¼Ò ¿îµ¿À» °í°­µµ¿Í º¸Åë °­µµ·Î ¹ø°¥¾Æ°¡¸ç ÇÏ¼¼¿ä.  °¡½¿ÀÌ ¶Ù¸ç ¾à°£ È£ÈíÀÌ °¡ºüÁö°í ¼Ò·®ÀÇ ¶¡ÀÌ ³ª´Â Àû´çÇÑ À¯»ê¼Ò ¿îµ¿Àº ÁÁÁö¸¸ ±Ù·Â ¿îµ¿ÀÌ³ª °ú°ÝÇÑ ¿îµ¿Àº Áö¾çÇÏ¿©¾ß ÇÕ´Ï´Ù. ¶ÇÇÑ ¸é¿ª°­È­¸¦ ½ÃÄÑÁÖ´Â ºí·çº£¸®¸¦ ²ÙÁØÈ÷ ¼·ÃëÇØÁÖ¼¼¿ä!."
-
- 
-
-            # hightemp / simgeungyungsaek 
-
- 
-
-        else:
-
- 
-
-            #print ("s3")
-
- 
-
-            resultCase = 2;
-
- 
-
-            s3Result = "O"
-
- 
-
-            s4Result = "X"
-
- 
-
-            disease = "±Þ¼º ½É±Ù°æ»ö, °íÇ÷¾Ð, ´ëµ¿¸Æ°ü ÇùÂøÁõ ¿¹»ó"
-
- 
-
-            advice = "¿ªµµ³ª ¾¾¸§ ½Ç³»°ñÇÁ µî°ú °°ÀÌ ÂªÀº ¼ø°£¿¡ °Ý·ÄÇÏ°Ô ÈûÀ» ¾²°Å³ª È£ÈíÀ» Á¤ÁöÇÑ »óÅÂ¿¡¼­ ÇàÇÏ´Â ¿îµ¿Àº ¼öÃà±â Ç÷¾Ð°ú ÀÌ¿Ï±â Ç÷¾ÐÀÇ »ó½Â ¹ÝÀÀÀÌ Å©°í ¸»ÃÊ Ç÷°ü ÀúÇ×µµ Áõ°¡½ÃÅ´À¸·Î ÇÇÇÏ´Â °ÍÀÌ ÁÁ½À´Ï´Ù. ±×·¯¹Ç·Î Ç÷¾×¼øÈ¯À» ¿øÈ°ÇÏ°Ô ÇÏ´Â °¡º­¿î ½ºÆ®·¹Äª, °È±â, ÀÚÀü°Å Å¸±â µîÀÇ ¿îµ¿À» ÃßÃµµå¸³´Ï´Ù. ¶ÇÇÑ Æ÷È­ Áö¹æÀÌ Àû°í ºÒÆ÷È­Áö¹æ»êÀÌ ¸¹Àº µîÇª¸¥ »ý¼±, ¿Ã¸®ºêÀ¯ µîÀ» ¼·ÃëÇÏ´Â °ÍÀÌ ÁÁÀ¸¸ç Ä®·ýÀÌ ¸¹Àº ºÎÃß, ½Ã±ÝÄ¡, Å°À§¸¦ ²ÙÁØÈ÷ ¼·ÃëÇØ ÁÖ¼¼¿ä!"
-
- 
-
-            # simbeujun / hyupsimjeung
-
- 
-
-            
-
- 
-
-    elif (endInd-startInd == 4):
-
- 
-
-          #print ("s3 and s4")
-
- 
-
-          s3Result = "O"
-
- 
-
-          s4Result = "O"
-
- 
-
-          disease = "½ÉºÎÀü, °ü»óµ¿¸ÆÁúÈ¯ ¿¹»ó"
-
- 
-
-          advice = "°íÇ÷¾Ð, ´ç´¢, ½ºÆ®·¹½º µîÀÌ µ¿¸Æ°æÈ­ÁõÀ» ÀçÃËÇÏ´Â ¿äÀÎÀÔ´Ï´Ù. µû¶ó¼­ ±Ý¿¬°ú Ã¼ÁßÁ¶Àý, Á¤»ó Ç÷¾Ð À¯Áö, ±×¸®°í ¾Æ½ºÇÇ¸° º¹¿ëÀ» ÀØÁö ¸»¾Æ¾ß ÇÕ´Ï´Ù. ¶ÇÇÑ Ç÷¾ÐÀ» ³ôÀÏ ¼ö ÀÖ´Â ³ªÆ®·ý°ú ¾ËÄÚ¿ÃÀ» Áö¾çÇÏ¸ç ¸¶½Ã´õ¶óµµ ÇÏ·ç¿¡ ¸ÆÁÖ 1Äµ, ¼ÒÁÖ 1~2ÀÜÀ¸·Î Á¦ÇÑÇØ¾ß ÇÕ´Ï´Ù!"
-
-          # simgeungyungsaek / simbeujun
-
- 
-
-            
-
- 
-
- 
-
- 
-
-if (beatPerMinute <= 60):
-
- 
-
-        heartage = "´ç½ÅÀÇ ½ÉÀå ³ªÀÌ´Â 20´ë ÀÔ´Ï´Ù."
-
- 
-
-elif (beatPerMinute <= 65):
-
- 
-
-        heartage = "´ç½ÅÀÇ ½ÉÀå ³ªÀÌ´Â 30´ë ÀÔ´Ï´Ù."
-
- 
-
-elif(beatPerMinute <= 70):
-
- 
-
-        heartage = "´ç½ÅÀÇ ½ÉÀå ³ªÀÌ´Â 40´ë ÀÔ´Ï´Ù."
-
- 
-
-elif(beatPerMinute <= 75):
-
- 
-
-        heartage = "´ç½ÅÀÇ ½ÉÀå ³ªÀÌ´Â 50´ë ÀÔ´Ï´Ù."
-
- 
-
-elif(beatPerMinute <= 80):
-
- 
-
-        heartage = "´ç½ÅÀÇ ½ÉÀå ³ªÀÌ´Â 60´ë ÀÔ´Ï´Ù."
-
- 
-
-else:  
-
- 
-
-        heartage = "´ç½ÅÀÇ ½ÉÀå ³ªÀÌ´Â 70´ë ÀÔ´Ï´Ù."
-
- 
-
- 
-
- 
-
-now = datetime.datetime.now()
-
- 
-
-#print (now) # 2018-07-28 12: 11: 32.669083
-
- 
-
- 
-
-nowDate = now.strftime('%Y-%m-%d')
-
- 
-
-#print (nowDate) # 2018-07-28
-
- 
-
- 
-
-nowTime = now.strftime('%H:%M:%S')
-
- 
-
-#print (nowTime) # 12:11:32
-
- 
-
- 
-
-nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S')
-
- 
-
- 
-
-print(s3Result)
-
- 
-
-print(s4Result)
-
- 
-
-print(beatPerMinute)
-
- 
-
-print(disease)
-
- 
-
-print(advice)
-
- 
-
-print(heartage)
-
- 
-
-print('{} ¡ÆC'.format(tmp))
+print('{} Â°C'.format(tmp))
 
  
 
